d4643fb0faa6b0795fb3edf4312a07d9ede8d797
Md Jahid Hasan <80060849+hmjahid@users.noreply.github.com>
2025-06-02 09:39:47 +0600

    Add practise
    Add new practise

---
+++ Week 1 - C/ternary.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+int main(void) {
+    int a = get_int("Enter a number: ");
+    a > 0 ? printf("greater than 0\n") : printf("less than or equal to 0\n");
+
+}
